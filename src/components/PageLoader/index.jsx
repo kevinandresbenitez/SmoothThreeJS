e097ab95476ffd0e9ae06021d7c60880663fd959
Benitez kevin <kevinescudoderoble@gmail.com>
2022-11-13 19:46:48 -0300

    Add page loader

---
+++ src/components/PageLoader/index.jsx
@@ -0,0 +1,28 @@
+import {useEffect,useRef,useState} from 'react';
+
+function PageLoader(){
+
+    const [isPageLoaded,setPageLoaded] = useState(false);
+    const pageLoaderDom = useRef();
+
+    useEffect(()=>{
+        // On load set animation for dissmis and delete 
+        window.addEventListener('load',()=>{
+            setPageLoaded(true);
+            setTimeout(()=>{
+                pageLoaderDom.current.style.display ='none'
+            },1000)
+        })
+    },[])
+
+    return(
+        <div ref={pageLoaderDom} className={`pageloader-container ${isPageLoaded ? 'hidde-pageloader':false}`}>
+            <span className="loader"></span>
+        </div>
+    )
+}
+
+
+
+
+export default PageLoader
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 // Import components
 import Canvas from '../../components/Canvas';
+import PageLoader from '../../components/PageLoader';
 
 function Home() {
   
   return(
     <div>
       <Canvas />
+      <PageLoader />
     </div>
   )
 }
