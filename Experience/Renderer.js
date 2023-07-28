bbc4e7635b94df810c818496f088a850e5208a9c
kevin Benitez <kevinescudoderoble@gmail.com>
2023-07-28 17:57:58 -0300

    Configure renderer

--- Experience/Renderer.js
+++ Experience/Renderer.js
@@ -6,11 +6,20 @@ export class Renderer{
     mainExperience;
     constructor(){
         this.mainExperience = new Experience();
-        this.renderer = new THREE.WebGLRenderer({canvas:this.mainExperience.canvas});
+        this.renderer = new THREE.WebGLRenderer({canvas:this.mainExperience.canvas,antialias:true});
     }
 
     configureRenderer(){
         this.resize();
+        this.renderer.useLegacyLight = true;
+        this.renderer.toneMapping = THREE.CineonToneMapping;
+        this.renderer.toneMappingExposure = 1.75;
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this.renderer.setClearColor('#dfe7e5');
+        this.renderer.setSize(Sizes.width,Sizes.height);
+        this.renderer.setPixelRatio(Sizes.pixelRatio);
+        this.renderer.outputColorSpace = THREE.SRGBColorSpace;
     }
 
     animate = ()=>{
@@ -21,5 +30,6 @@ export class Renderer{
 
     resize(){
         this.renderer.setSize(Sizes.width,Sizes.height);
+        this.renderer.setPixelRatio(Sizes.pixelRatio);
     }
 }
